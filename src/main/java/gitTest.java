ddf615c677091fd7a5a947880ebfcc4956f7af67
SL <SL@lxt.com>
2021-04-15 21:53:05 +0800

    first commit

---
+++ src/main/java/gitTest.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("赵航");
+        System.out.println("神龙");
+    }
+}
